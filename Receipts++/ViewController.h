37ad0cad3698175e2b2d9ef54de1a8c5f9cf7ced
Alex Robertson <hello_im_alex@hotmail.com>
2015-11-12 10:58:42 -0800

    initial commit

---
+++ Receipts++/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts++
+//
+//  Created by Alex on 2015-11-12.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
